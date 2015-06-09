27eec35c01b6da82e5f016aacaaedcde3e9ac6d1
XiongMing <chexming@gmail.com>
2015-06-09 10:35:59 +0800

    submit for save

--- main.cpp
+++ main.cpp
@@ -1,9 +1,46 @@
 #include <stdio.h>
+#include "sqlite3.h"
 #include "mongoose.h"
 
-int main(int argc, char *argv[])
-{
-	printf("haha");
+static int callback(void *NotUsed, int argc, char **argv, char **azColName){
+	int i;
+	for(i=0; i<argc; i++){
+		printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+	}
+	printf("\n");
+	return 0;
+}
+
+int main(int argc, char **argv){
+	sqlite3 *db;
+	char *zErrMsg = 0;
+	int rc;
+
+	if( argc!=3 ){
+		fprintf(stderr, "Usage: %s DATABASE SQL-STATEMENT\n", argv[0]);
+		return(1);
+	}
+	rc = sqlite3_open(argv[1], &db);
+	if( rc ){
+		fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+		sqlite3_close(db);
+		return(1);
+	}
+	rc = sqlite3_exec(db, argv[2], callback, 0, &zErrMsg);
+	if( rc!=SQLITE_OK ){
+		fprintf(stderr, "SQL error: %s\n", zErrMsg);
+		sqlite3_free(zErrMsg);
+	}
+	sqlite3_close(db);
+
+	struct mg_server *server = mg_create_server(NULL, NULL);
+	mg_set_option(server, "document_root", ".");      // Serve current directory
+	mg_set_option(server, "listening_port", "8080");  // Open port 8080
+
+	for (;;) {
+		mg_poll_server(server, 1000);   // Infinite loop, Ctrl-C to stop
+	}
+	mg_destroy_server(&server);
 
 	return 0;
 }
